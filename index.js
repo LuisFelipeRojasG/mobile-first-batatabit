6c49dd6075f9a3579a9a4772ce20395980532c24
LuisFelipeRojas <luisfeliperojas27@yahoo.es>
2022-07-24 12:11:26 -0500

    Slider plans

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-const cardArrowLeft = document.getElementsByClassName("arrowLeft");
-const cardArrowRight = document.getElementsByClassName("arrowRight");
+const sliderPlans = document.getElementById("slider_plans");
+const containerCards = document.querySelectorAll(".container-card");
+const containerCardsLast = containerCards[containerCards.length - 1];
 
-const listCardPlan = ["plan_annual", "plan_biannual", "plan_quarterly"];
-let index = 0;
-const cardActive = () => listCardPlan[index].classList.add('active')
+const planArrowLeft = document.querySelector(".arrowLeftPlans");
+const planArrowRight = document.querySelector(".arrowRightPlans");
 
-cardArrowLeft.addEventListener('click', () => {
-    
-    switch (index) {
-        case 0:
-            index++
-        break;
-        case 1:
-            index++
-        break;
-    }
-    return(
-        cardActive()
-    );
-    
+sliderPlans.insertAdjacentElement('afterbegin', containerCardsLast);
+
+function moveNext() {
+  let containerCardsFirst = document.querySelectorAll(".container-card")[0];
+  sliderPlans.style.marginLeft = "-200%";
+  sliderPlans.style.transition = "all .5s";
+  setTimeout(() => {
+    sliderPlans.style.transition = "none";
+    sliderPlans.insertAdjacentElement('beforeend', containerCardsFirst);
+    sliderPlans.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function movePrev() {
+  const containerCards = document.querySelectorAll(".container-card");
+  const containerCardsLast = containerCards[containerCards.length - 1];
+  
+  sliderPlans.style.marginLeft = "0";
+  sliderPlans.style.transition = "all .5s";
+  setTimeout(() => {
+    sliderPlans.style.transition = "none";
+    sliderPlans.insertAdjacentElement('afterbegin', containerCardsLast);
+    sliderPlans.style.marginLeft = "-100%";
+  }, 500);
+}
+
+planArrowRight.addEventListener('click', () => {
+  console.log("click");
+  moveNext()
 })
 
-
-
-
+planArrowLeft.addEventListener('click', () => {
+  console.log("click");
+  movePrev()
+})
