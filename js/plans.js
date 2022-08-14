3388c1f5125796960f1a8467a11416b60c483c7c
LuisFelipeRojas <luisfeliperojas27@yahoo.es>
2022-08-14 17:26:22 -0500

    Mobile first listo01

--- js/plans.js
+++ js/plans.js
@@ -9,12 +9,12 @@ sliderPlans.insertAdjacentElement('afterbegin', containerCardsLast);
 
 function moveNext() {
   let containerCardsFirst = document.querySelectorAll(".container-card")[0];
-  sliderPlans.style.marginLeft = "-200%";
+  sliderPlans.style.marginLeft = "-100%";
   sliderPlans.style.transition = "all .5s";
   setTimeout(() => {
     sliderPlans.style.transition = "none";
     sliderPlans.insertAdjacentElement('beforeend', containerCardsFirst);
-    sliderPlans.style.marginLeft = "-100%";
+    sliderPlans.style.marginLeft = "0";
   }, 500);
 }
 
@@ -22,12 +22,12 @@ function movePrev() {
   const containerCards = document.querySelectorAll(".container-card");
   const containerCardsLast = containerCards[containerCards.length - 1];
   
-  sliderPlans.style.marginLeft = "0";
+  sliderPlans.style.marginLeft = "100%";
   sliderPlans.style.transition = "all .5s";
   setTimeout(() => {
     sliderPlans.style.transition = "none";
     sliderPlans.insertAdjacentElement('afterbegin', containerCardsLast);
-    sliderPlans.style.marginLeft = "-100%";
+    sliderPlans.style.marginLeft = "0";
   }, 500);
 }
 
